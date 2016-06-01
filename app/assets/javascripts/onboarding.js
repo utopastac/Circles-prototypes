4271c626756dea739464048f0246a005260b3822
Peter Wright <utopastac@gmail.com>
2016-06-01 22:29:27 +0100

    Mapping out routes for the first true MVP

---
+++ app/assets/javascripts/onboarding.js
@@ -0,0 +1,26 @@
+$(function(){ 
+  var questionHolder = $("#onboarding");
+  var questions = $("section", questionHolder);
+  var next = $(".next", questionHolder);
+  var current = 0;
+  next.click(nextQuestion);  
+  if(questions.length > 0) setUpQuestions();
+
+  function setUpQuestions(){
+    //TweenMax.set(questionHolder, {minHeight: $(window).height() - navHeight + 70});
+    TweenMax.set(questions, {autoAlpha: 0, x: 150, display: "none"});
+    TweenMax.set(questions.eq(0), {autoAlpha:1, x: 0, display: "block"});
+  }
+
+  function showQuestion(target){
+    TweenMax.to(questions.eq(current), 0.4, {autoAlpha:0, x: -150, display: "none", ease:Power2.easeOut});
+    TweenMax.to(questions.eq(target), 0.5, {autoAlpha:1, x: 0, display: "block", ease:Power2.easeInOut});
+    current = target;
+  }
+
+  function nextQuestion(event){
+    showQuestion(current + 1);
+    return false;
+  }
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root 'homepage#index'
 
-  get '/circles', to: 'circles#index', as: 'circles'
+  get '/intro', to: 'circles#intro', as: 'intro'
+  get '/status', to: 'circles#status', as: 'status'
+  get '/onboarding', to: 'circles#onboarding', as: 'onboarding'
+  get '/onboarding-support', to: 'circles#onboarding-support', as: 'onboarding-support'
+
+  get '/network', to: 'circles#network', as: 'network'
+  get '/account', to: 'circles#account', as: 'account'
+  get '/notifications', to: 'circles#notifications', as: 'notifications'
+  get '/circles', to: 'circles#circles', as: 'circles'
+  get '/circle', to: 'circles#circle', as: 'circle'
+
+
 end
