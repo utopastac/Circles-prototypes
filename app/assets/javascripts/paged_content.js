52ff8aab0c46a1a48b408c2f24db778f0d8868f4
Peter Wright <utopastac@gmail.com>
2016-06-02 22:04:52 +0100

    Adding in the basic screens needed, structure coming soon

---
+++ app/assets/javascripts/paged_content.js
@@ -0,0 +1,34 @@
+$(function(){ 
+  // var pageHolder = $(".paged-content");
+  // var pages = $("section", pageHolder);
+  // var pagelinks = $("[data-page-target]", questionHolder);
+  // var current = 0;
+  // var history = []
+  // pagelinks.click(nextQuestion);  
+  // if(questions.length > 0) setUpQuestions();
+
+  // function setUpPages(){
+  //   //TweenMax.set(questionHolder, {minHeight: $(window).height() - navHeight + 70});
+  //   TweenMax.set(pages, {autoAlpha: 0, x: 200, display: "none"});
+  //   TweenMax.set(pages.eq(0), {autoAlpha:1, x: 0, display: "block"});
+  // }
+
+  // function showPage(target){
+  //   TweenMax.to(pages.eq(current), 0.4, {autoAlpha:0, x: -200, display: "none", ease:Power2.easeOut});
+  //   TweenMax.to(pages.eq(target), 0.4, {autoAlpha:1, x: 0, display: "block", ease:Power2.easeOut});
+  //   current = target;
+  // }
+
+  // function goToPage(event){
+  //   var elem = $(event.currentTarget);
+  //   var pageTarget = $(elem.data("page-target"));
+  //   var target = pages.index(pageTarget);
+  //   showPage(target);
+  //   return false;
+  // }
+
+  // function back(event){
+
+  // }
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
 
   get '/network', to: 'circles#network', as: 'network'
   get '/account', to: 'circles#account', as: 'account'
+  get '/account_profile', to: 'circles#account_profile', as: 'account_profile'
+  get '/account_readme', to: 'circles#account_readme', as: 'account_readme'
   get '/notifications', to: 'circles#notifications', as: 'notifications'
   get '/circles', to: 'circles#circles', as: 'circles'
   get '/circle', to: 'circles#circle', as: 'circle'
+  get '/circle_private', to: 'circles#circle_private', as: 'circle_private'
 
 
 end
