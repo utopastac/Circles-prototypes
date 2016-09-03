c77101edcf3fc08a80911b30fb74b6c41852dac3
Peter Wright <utopastac@gmail.com>
2016-09-03 08:27:49 +0100

    Putting in the structure for circle creation

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,3 +1,23 @@
 class CirclesController < PublicController
   layout "circles"
+  PEOPLE = ["Coral Wheeler",
+"Sudie Koening",
+"Lindsay Bussard",
+"Eliza Hajduk",
+"Allison Becton",
+"Helen Conlin",
+"Wally Martucci",
+"Evalyn Gum",
+"Hans Prisco",
+"Asia Waugh",
+"Marquetta Shears",
+"Ivory Croghan",
+"Debbra Pruitt",
+"Bryant Chamorro",
+"Tawanna Gault",
+"Janelle Buffkin",
+"Willis Shield",
+"Garth Pink",
+"Azucena Alarcon",
+"Brinda Nolte"]
 end
