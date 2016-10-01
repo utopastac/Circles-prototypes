403481f9dcdc93d82fa8d9a5943e77ab67d264e8
Peter Wright <utopastac@gmail.com>
2016-10-01 14:22:34 +0100

    Making the logo an svg

--- app/assets/javascripts/post-reaction.js
+++ app/assets/javascripts/post-reaction.js
@@ -1,28 +1,34 @@
 $(function(){ 
   var reactionButton = $(".reaction-button");
-  var reactionHolder = $(".reactions-holder");
-  var reactions = $(".reactions-holder img");
-  var tl = new TimelineLite;
   var on = false;
 
+  var reactionHolder = $(".reactions-holder");
+  var reactions = $(".reactions-holder img");
   TweenMax.set(reactionHolder, {autoAlpha: 0, scale: 0.1, display: "none"});
   TweenMax.set(reactions, {autoAlpha: 0, scale: 0.1});
 
   reactionButton.on("tap", function(event){
+    var elem = $(event.currentTarget).closest(".reactions");
     if(!on){
-      showReactions();
+      showReactions(elem);
     } else {
-      hideReactions();
+      hideReactions(elem);
     }
     on = !on;
   });
 
-  function showReactions(){
+  function showReactions(elem){
+    var reactionHolder = $(".reactions-holder", elem);
+    var reactions = $(".reactions-holder img", elem);
+    var tl = new TimelineLite;
     TweenMax.to(reactionHolder, 0.3, {autoAlpha: 1, scale: 1, transformOrigin:"0% 50% -50", display: "flex", ease:Back.easeOut});
     tl.staggerTo(reactions, 0.3, {autoAlpha: 1, scale: 1, ease:Back.easeOut}, 0.05, "+=0");
   }
 
-  function hideReactions(){
+  function hideReactions(elem){
+    var reactionHolder = $(".reactions-holder", elem);
+    var reactions = $(".reactions-holder img", elem);
+    var tl = new TimelineLite;
     TweenMax.to(reactionHolder, 0.25, {autoAlpha: 0, scale: 0.1, transformOrigin:"0% 50% -50", display: "none"});
     tl.staggerTo(reactions, 0.3, {autoAlpha: 0, scale: 0.1, ease:Back.easeOut}, 0.1, "+=0");
   }
